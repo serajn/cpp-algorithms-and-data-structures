ee3f889aeb0f3aa385c4d14ee13ab87a3c91505e
Seraj Noureddine <snoureddine01@outlook.com>
2026-07-01 01:18:53 -0400

    Setup for the two-sum sorted input array problem

---
+++ leetcode/medium/two_sum_sorted_input_array.cpp
@@ -0,0 +1,28 @@
+# include <iostream>
+# include <vector>
+
+using namespace std;
+
+class Solution {
+    public:
+        vector<int> twoSum(vector<int>& numbers, int target) {
+            
+        }
+};
+
+
+int main() {
+
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+
+    Solution solution;
+    vector<int> result = solution.twoSum(nums, target);
+
+    for (int i : result) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    return 0;
+}   
