7abe647be7b3beb6b04229c9ee37866113a9347e
Seraj Noureddine <snoureddine01@outlook.com>
2026-07-01 01:32:58 -0400

    Implemented index pointers to allow comparison of elements in O(1) space

--- leetcode/medium/two_sum_sorted_input_array.cpp
+++ leetcode/medium/two_sum_sorted_input_array.cpp
@@ -6,7 +6,25 @@ using namespace std;
 class Solution {
     public:
         vector<int> twoSum(vector<int>& numbers, int target) {
+            int index_1 = 0;
+            int index_2 = numbers.size() - 1;
             
+            while (index_1 != index_2) {
+                int sum = numbers[index_1] + numbers[index_2];
+
+                if (sum == target) {
+                    return {index_1 + 1, index_2 + 1};
+                }
+
+                if (sum < target) {
+                    index_1++;
+                }
+                else {
+                    index_2--;
+                }
+            }
+
+            return {};
         }
 };
 
