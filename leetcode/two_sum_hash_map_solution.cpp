d5f59b047c338bced4cf1616426a8815164801a6
Seraj Noureddine <snoureddine01@outlook.com>
2026-06-30 23:18:03 -0400

    Implemented hashmap to store complement-index pairs

--- leetcode/two_sum_hash_map_solution.cpp
+++ leetcode/two_sum_hash_map_solution.cpp
@@ -1,12 +1,28 @@
 # include <iostream>
 # include <vector>
+# include <unordered_map>
 
 using namespace std;
 
 class Solution {
     public:
-        vector<int> twoSum(vector<int>& nums, int target) {
+    
+        unordered_map<int, int> mp;
 
+        vector<int> twoSum(vector<int>& nums, int target) {
+            for (int i = 0; i < nums.size(); i++) {
+
+                int complement = target - nums[i];
+
+                auto it = mp.find(complement);
+
+                if (it != mp.end()) {
+                    return {it->second, i};
+                }
+
+                mp[nums.at(i)] = i;
+            }
+            return {};
         }
 };
 
